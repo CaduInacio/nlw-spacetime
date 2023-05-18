d740731e82dd8119f633930595cc50457edb70c2
Carlos Eduardo Inácio <carloseduardo.inacio.56@gmail.com>
2023-05-18 10:58:21 -0300

    Aula 2 finalizada

---
+++ mobile/src/@types/assets.d.ts
@@ -0,0 +1,12 @@
+/* Importações de PNG */
+
+declare module '*.png'
+
+/* Importações de SVG */
+
+declare module '*.svg' {
+  import React from 'react'
+  import { SvgProps } from 'react-native-svg'
+  const content: React.FC<SvgProps>
+  export default content
+}
