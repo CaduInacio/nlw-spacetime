001811963a0067762b3183165ae28bbd2841e04d
Carlos Eduardo Inácio <carloseduardo.inacio.56@gmail.com>
2023-05-19 17:20:56 -0300

    Aula 3 finalizada

---
+++ web/src/app/api/auth/callback/route.ts
@@ -0,0 +1,25 @@
+import { api } from '@/app/lib/api'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url)
+  console.log(searchParams)
+
+  const code = searchParams.get('code')
+
+  const registerResponse = await api.post('/register', {
+    code,
+  })
+
+  const { token } = registerResponse.data
+
+  const redirectURL = new URL('/', request.url)
+
+  const secondsToExpire = 60 * 60 * 24 * 7
+
+  return NextResponse.redirect(redirectURL, {
+    headers: {
+      'Set-Cookie': `token=${token}; Path=/; max-age=${secondsToExpire}`,
+    },
+  })
+}
